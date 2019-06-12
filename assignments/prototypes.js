ba39059bffbd7ed1aa79a4d48cd1016e9cd96ca7
Marques Johnson <marques.johnson1992@gmail.com>
2019-06-12 18:15:54 -0400

    final commit

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -15,13 +15,33 @@
   * dimensions (These represent the character's size in the video game)
   * destroy() // prototype method that returns: `${this.name} was removed from the game.`
 */
+  function GameObject(attributes) {
+    this.createdAt = attributes.createdAt
+    this.name = attributes.name
+    this.dimensions = attributes.dimensions
+    };
 
+    GameObject.prototype.destroy = function() {
+      return `${this.name} was removed from the GameObject.`
+    }
+  
 /*
   === CharacterStats ===
   * healthPoints
   * takeDamage() // prototype method -> returns the string '<object name> took damage.'
   * should inherit destroy() from GameObject's prototype
 */
+
+function CharacterStats(attributes) {
+  GameObject.call(this, attributes)
+  this.healthPoints = attributes.healthPoints
+
+}
+CharacterStats.prototype = Object.create(GameObject.prototype)
+
+CharacterStats.prototype.takeDamage = function() {
+  return `${this.name} took damage.`
+}
 
 /*
   === Humanoid (Having an appearance or character resembling that of a human.) ===
@@ -32,6 +52,22 @@
   * should inherit destroy() from GameObject through CharacterStats
   * should inherit takeDamage() from CharacterStats
 */
+
+function Humanoid (attributes) {
+  CharacterStats.call(this, attributes)
+  this.team = attributes.team
+  this.weapons = attributes.weapons
+  this.language = attributes.language
+
+}
+
+Humanoid.prototype = Object.create(CharacterStats.prototype)
+
+Humanoid.prototype.greet = function() {
+  return `${this.name} offers a greeting in ${this.language}`
+}
+
+
  
 /*
   * Inheritance chain: GameObject -> CharacterStats -> Humanoid
@@ -39,9 +75,10 @@
   * Instances of CharacterStats should have all of the same properties as GameObject.
 */
 
+
 // Test you work by un-commenting these 3 objects and the list of console logs below:
 
-/*
+
   const mage = new Humanoid({
     createdAt: new Date(),
     dimensions: {
@@ -102,7 +139,7 @@
   console.log(archer.greet()); // Lilith offers a greeting in Elvish.
   console.log(mage.takeDamage()); // Bruce took damage.
   console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-*/
+
 
   // Stretch task: 
   // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
--- assignments/this.js
+++ assignments/this.js
@@ -1,26 +1,83 @@
-/* The for principles of "this";
-* in your own words. explain the four principle for the "this" keyword below.
+/*The for principles of "this";
+// in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Implicit Binding- look to the left of the dot at call time
+* 2  Explicit Binding- call, apply, bind
+* 3. new Binding- 
+* 4. window Binding- last Option, unless in strict mode.
 *
 * write out a code example of each explanation above
 */
+
 
 // Principle 1
 
 // code example for Window Binding
 
+let sayAge = function() {
+    console.log(this.age);
+};
+
+let me = {
+    age: 25
+};
+
+sayAge();
+window.age = 35;
+sayAge();
+
 // Principle 2
 
 // code example for Implicit Binding
 
+let Person = function(name, age) {
+    return {
+        name: name,
+        age: age,
+        sayName: function() {
+            console.log(this.name);
+        },
+        mother: {
+            name: "TRD",
+            sayName: function() {
+                console.log(this.name);
+            }
+        }
+    };
+};
+
+let Marques = Person('Marques', 37);
+Marques.SayName();
+Marques.mother.sayName();
+
+
+
 // Principle 3
 
 // code example for New Binding
+let Animal = function(color, name, type){
+    this.color = color;
+    this.name = name;
+    this.type = type;
+};
+
+let zebra = new Animal('black and white', 'Zorro', 'Zebra');
+
 
 // Principle 4
 
 // code example for Explicit Binding
+
+let sayName = function(things1, things2, things3) {
+    console.log("My name is " + this.name + ' and I know ' + things1 + ', ' + things2 + ', and ' + things3);
+};
+
+let trd = function() {
+    name: "TRD"
+    age: 5000
+};
+
+
+let things = ["Files", "People", "keys"];
+
+sayName.call(trd,things[0], things[1], things[2]);
